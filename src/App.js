c597004190fa500cb738037869c8637ea1651a60
Simon <simon@simonhamilton.ca>
2018-03-09 18:35:19 +0000

    added image to index page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+import beer from './images/beer.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={beer} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. Stuff
         </p>
+        <div> <img src={beer} alt="beer" /> </div>
       </div>
     );
   }
