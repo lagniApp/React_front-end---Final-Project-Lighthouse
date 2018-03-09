a7e233844eb8bcba5c4add0df40b11804c35bcca
Simon <simon@simonhamilton.ca>
2018-03-09 23:25:01 +0000

    coupon styling and index background

--- src/components/App.js
+++ src/components/App.js
@@ -21,20 +21,62 @@ class App extends Component {
           <img src={beer} className="App-logo" alt="logo" />
           <h1 className="App-title">Lagni App</h1>
         </header>
-        <p className="App-intro">
-         Welcome
-        </p>
-        <button type="button"><img src={beer} alt="beer"/></button>
-        <button type="button"><img src={wine} alt="wine"/></button>
-        <button type="button"><img src={cocktail} alt="cocktail"/></button>
-        <button type="button"><img src={food} alt="all food"/></button>
-        <button type="button"><img src={pizza} alt="pizza"/></button>
-        <button type="button"><img src={burrito} alt="burrito"/></button>
-        <button type="button"><img src={hamburger} alt="hamburger"/></button>
-        <button type="button"><img src={sushi} alt="sushi"/></button>
-        <button type="button"><img src={pasta} alt="pasta"/></button>
-        <button type="button"><img src={steak} alt="steak"/></button>
+        <div class="page-container">
+          <div className="App-intro">Welcome</div>
+          <div class="image-buttons">
+            <button type="button"><img src={beer} alt="beer"/></button>
+            <button type="button"><img src={wine} alt="wine"/></button>
+            <button type="button"><img src={cocktail} alt="cocktail"/></button>
+            <button type="button"><img src={food} alt="all food"/></button>
+            <button type="button"><img src={pizza} alt="pizza"/></button>
+            <button type="button"><img src={burrito} alt="burrito"/></button>
+            <button type="button"><img src={hamburger} alt="hamburger"/></button>
+            <button type="button"><img src={sushi} alt="sushi"/></button>
+            <button type="button"><img src={pasta} alt="pasta"/></button>
+            <button type="button"><img src={steak} alt="steak"/></button>
+          </div>
+          <div class="search-bar">
+            <input type="text" placeholder="Search.."/>
+          </div>
+        
+          <div class="coupon-container">
+            <div class="coupon-image-tags">
+              <img src={beer} alt="beer"/>
+              <img src={beer} alt="beer"/>
+              <img src={beer} alt="beer"/>
+              <img src={beer} alt="beer"/>
+            </div>
+            <div class="restaurant-name"><h3> Restaurant Name </h3></div>
+            <div class="coupon-info"> Coupon Info and more stuff </div>
+            <button type="button">Map</button>
+            <button type="button">Get Coupon</button>
+          </div>
 
+          <div class="coupon-container">
+            <div class="coupon-image-tags">
+              <img src={wine} alt="beer"/>
+              <img src={pizza} alt="beer"/>
+              <img src={sushi} alt="beer"/>
+            </div>
+            <div class="restaurant-name"><h3> Crazy Johnnys </h3></div>
+            <div class="coupon-info"> Half off Everything!  What a crazy place!! </div>
+            <button type="button">Map</button>
+            <button type="button">Get Coupon</button>
+          </div>
+
+          <div class="coupon-container">
+            <div class="coupon-image-tags">
+              <img src={burrito} alt="beer"/>
+              <img src={hamburger} alt="beer"/>
+            </div>
+            <div class="restaurant-name"><h3> Big Sallys Really Long Restaurant Name </h3></div>
+            <div class="coupon-info"> Double up!  Buy a burger and get a free BuRRRIto</div>
+            <button type="button">Map</button>
+            <button type="button">Get Coupon</button>
+          </div>
+
+
+        </div>
       </div>
     );
   }
