8ccd3daa3621c10c83e898c13d1431a6b642be51
vagrant <kennethracadio@hotmail.com>
2018-03-10 18:26:40 +0000

    Integrated html/css to coupon

--- src/components/Coupons/Coupon.jsx
+++ src/components/Coupons/Coupon.jsx
@@ -3,15 +3,15 @@ import {Row, Col, PageHeader, Table} from 'react-bootstrap'
 import {Route, Switch, Link} from 'react-router-dom'
 
 import Beer from '../../images/beer.png'
-import Wine from '../../images/wine-glass.png'
-import Cocktail from '../../images/cocktail.png'
-import Pizza from '../../images/pizza.png'
-import Food from '../../images/cutlery.png'
-import Burrito from '../../images/burrito.png'
-import Hamburger from '../../images/hamburger.png'
-import Pasta from '../../images/spaghetti.png'
-import Sushi from '../../images/sushi.png'
-import Steak from '../../images/steak.png'
+import wine from '../../images/wine-glass.png'
+import cocktail from '../../images/cocktail.png'
+import pizza from '../../images/pizza.png'
+import food from '../../images/cutlery.png'
+import burrito from '../../images/burrito.png'
+import hamburger from '../../images/hamburger.png'
+import pasta from '../../images/spaghetti.png'
+import sushi from '../../images/sushi.png'
+import steak from '../../images/steak.png'
 
 
 
@@ -20,29 +20,18 @@ class Coupon extends React.Component {
   render() {
     const coupon = this.props.coupon
     return (
-      // <div>
-      //   <div>------</div>
-      //   <div>{this.props.coupon.description}</div>
-      //   {/*<div>{JSON.stringify(this.props.coupon)}</div>*/}
-      // </div>
 
       <div class="coupon-container">
-            <div class="coupon-image-tags">
-            {coupon.tags.map((tag) => {
-              console.log("TAGGG", tag.cuisine)
-              const style = `${tag.cusine}`
-              const img = `${this.props.url}`;
-              return <img src={style} alt="{tag.cuisine}"/>
-            })}
-
-            </div>
-            <div class="restaurant-name"><h3> {coupon.restaurant.name} </h3></div>
-            <div class="coupon-info"> {coupon.description} </div>
-            <button type="button">Map</button>
-            <button type="button">Get Coupon</button>
-          </div>
-
-
+        <div class="coupon-image-tags">
+          {coupon.tags.map((tag) => {
+            return <img src={Beer} />
+          })}
+        </div>
+        <div class="restaurant-name"><h3> {coupon.restaurant.name} </h3></div>
+        <div class="coupon-info"> {coupon.description} </div>
+        <button type="button">Map</button>
+        <button type="button">Get Coupon</button>
+        </div>
     )
   }
 }
