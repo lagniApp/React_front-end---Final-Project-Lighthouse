dab7a00552a972e84e5a70dd8eaebb8c3ae5db1b
tjbeirao <tjbeirao@gmail.com>
2018-03-12 19:12:35 -0400

    restaurant page rendering meetup page

--- src/components/Restaurants/Restaurant/Restaurant.jsx
+++ src/components/Restaurants/Restaurant/Restaurant.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+// import Navigation from 'react-toolbox/lib/navigation';
+// import Link from 'react-toolbox/lib/link';
 import Resource from '../../../models/resource'
-// import MeetUp from './MeetUp'
+
+import MeetUp from './MeetUp'
 const RestaurantId = Resource('restaurants')
 
+class RestaurantePage extends React.Component {
+    render() {
+        return (
+            <button {...this.props}>
+                Coupons
+            </button>
+        );
+    }
+}
+class Coupon extends React.Component {
+    render() {
+        return (
+            <button {...this.props}>
+                Coupons
+            </button>
+        );
+    }
+}
 
+class Statistic extends React.Component {
+    render() {
+        return (
+            <button {...this.props}>
+                Statistic
+            </button>
+        );
+    }
+}
 class Restaurant extends React.Component {
     constructor(props) {
         super(props)
@@ -11,13 +41,14 @@ class Restaurant extends React.Component {
             results: "",
             restaurantId: (this.props.match.params.id || null),
             show: false,
-            redirect: ''
+            redirect: '',
+            clicked: false
         }
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentWillMount() {
         if (!this.state.restaurantId) return
-        console.log("IF VALID")
         RestaurantId.find(this.props.match.params.id)
             .then((result) => {
                 this.setState({
@@ -25,24 +56,51 @@ class Restaurant extends React.Component {
                     errors: null,
                     show: true,
                     redirect: '',
+                    meets: result.meetups
                 })
-                console.log("TEST", result)}
+                // console.log(result.meetups)
+                }
         )
             .catch((errors) => this.setState({ errors: errors }))
     }
 
-    // _hide = () => {
-    //     this.setState({ show: false, redirect: 'http://localhost:3002/' })
-    // }
+    handleClick() {
+        this.setState({
+            clicked: true
+        });
+    }
 
     render() {
         return (
             <div>
+                <p>
                 {this.state.results.name}
-                {/* {MeetUp} */}
+                </p>
+                <p>
+                {this.state.results.phone}
+                </p>
+                <p>
+                {this.state.results.address}
+                </p>
+                <p>
+                {this.state.results.balance}
+                </p>
+                <MeetUp meets={this.state.meets} />
+
+                {/* <div>
+                    <Coupon onClick={this.handleClick} />
+                    {this.state.clicked ? <Redirect /> : MeetUp}
+                    </div>
+                    <div>
+                    <Statistic onClick={this.handleClick} />
+                    {this.state.clicked ? <NewComponent /> : MeetUp}
+                </div> */}
             </div>
         )
+        
+        // ReactDOM.render(element, MeetUp.getElementById('meetup'));
     }
 }
+
 
 export default Restaurant
