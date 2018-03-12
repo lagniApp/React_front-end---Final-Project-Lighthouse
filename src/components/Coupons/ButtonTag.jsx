a02d17f3c38024406079201ab725dd3eb252d958
vagrant <kennethracadio@hotmail.com>
2018-03-12 15:43:35 +0000

    Change tagged button on click

--- src/components/Coupons/ButtonTag.jsx
+++ src/components/Coupons/ButtonTag.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 
 class ButtonTag extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tagStyle: ""
+    }
+  }
+
 
   render() {
     return (
-        <button type="button" onClick={this._tagClicked} value={this.props.tagName}>
+        <button type="button" onClick={this._tagClicked} value={this.props.tagName} className={this.state.tagStyle}>
         <img src={this.props.tagImg} alt={this.props.tagName}/>
         </button>
     )
   }
 
   _tagClicked = (e) => {
+    // change style of tag when clicked
+    this.state.tagStyle?  this.setState({tagStyle: ""}) : this.setState({tagStyle: "button-tag"});
+    // grab the type of tag that was clicked
     const tagVal = e.currentTarget.value
     let tags = []
     for (let coupon of this.props.coupons) {
       for (let tag of coupon.tags) {
           let couptag = tag.cuisine.toLowerCase();
+        // compare a coupon's tags to the tag that was clicked
         if (tagVal === couptag) {
           coupon.filter ? coupon.filter = false : coupon.filter = true;
           tags.push(coupon);
@@ -23,6 +34,7 @@ class ButtonTag extends React.Component {
       }
     }
     this.props.toggleTag(tags)
+
   }
 }
 
